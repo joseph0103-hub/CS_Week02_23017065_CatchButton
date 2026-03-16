cc6d74c409015dee6cc8ce524fd3f78284323126
Alfred <sdlee@wylie.co.kr>
2026-03-16 15:31:28 +0900

    배경색상 지정, 경고창 가운데 정렬

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -19,27 +19,41 @@ namespace CatchButton
             this.buttonCatch = new System.Windows.Forms.Button();
             this.buttonReset = new System.Windows.Forms.Button();
             this.SuspendLayout();
-
+            // 
+            // buttonCatch
+            // 
+            this.buttonCatch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonCatch.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold);
             this.buttonCatch.Location = new System.Drawing.Point(180, 160);
             this.buttonCatch.Name = "buttonCatch";
             this.buttonCatch.Size = new System.Drawing.Size(170, 70);
+            this.buttonCatch.TabIndex = 0;
             this.buttonCatch.Text = "나를 잡아봐";
             this.buttonCatch.UseVisualStyleBackColor = true;
             this.buttonCatch.Click += new System.EventHandler(this.buttonCatch_Click);
             this.buttonCatch.MouseEnter += new System.EventHandler(this.buttonCatch_MouseEnter);
-
-            this.buttonReset.Location = new System.Drawing.Point(650, 380);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonReset.Location = new System.Drawing.Point(630, 380);
             this.buttonReset.Name = "buttonReset";
-            this.buttonReset.Size = new System.Drawing.Size(120, 40);
+            this.buttonReset.Size = new System.Drawing.Size(140, 44);
+            this.buttonReset.TabIndex = 1;
             this.buttonReset.Text = "다시 시작";
             this.buttonReset.UseVisualStyleBackColor = true;
             this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
-
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonCatch);
             this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonCatch);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "버튼 잡기 게임";
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
 using System.Media;
 using System.Windows.Forms;
@@ -10,6 +12,8 @@ namespace CatchButton
     public partial class Form1 : Form
     {
         private readonly Random random = new Random();
+        private readonly List<Point> backgroundDots = new List<Point>();
+
         private int score = 0;
         private int missCount = 0;
         private Size initialCatchButtonSize;
@@ -21,11 +25,13 @@ namespace CatchButton
         public Form1()
         {
             InitializeComponent();
+
             initialCatchButtonSize = buttonCatch.Size;
             InitializeSounds();
             UpdateTitle();
         }
 
+        // 버튼 도망시, 버튼을 잡았을 때의 사운드 초기화
         private void InitializeSounds()
         {
             try
@@ -53,6 +59,7 @@ namespace CatchButton
             }
         }
 
+        // 버튼을 잡았을 때, 사운드 출력
         private void PlayCatchSound()
         {
             try
@@ -68,6 +75,7 @@ namespace CatchButton
             }
         }
 
+        // 버튼 도망시 사운드 출력
         private void PlayEscapeSound()
         {
             try
@@ -83,15 +91,16 @@ namespace CatchButton
             }
         }
 
+        //점수 차감, 놓침 횟수 증가 시키기
         private void UpdateTitle()
         {
-            this.Text = $"버튼 위치: ({buttonCatch.Left}, {buttonCatch.Top}) | 점수: {score} | 놓침: {missCount}/20";
+            Text = $"버튼 위치: ({buttonCatch.Left}, {buttonCatch.Top}) | 점수: {score} | 놓침: {missCount}/20";
         }
 
         private Point GetRandomLocationInsideForm(Control control)
         {
-            int maxX = Math.Max(0, this.ClientSize.Width - control.Width);
-            int maxY = Math.Max(0, this.ClientSize.Height - control.Height - 70);
+            int maxX = Math.Max(0, ClientSize.Width - control.Width);
+            int maxY = Math.Max(0, ClientSize.Height - control.Height - 70);
 
             int x = random.Next(0, maxX + 1);
             int y = random.Next(0, maxY + 1);
@@ -133,7 +142,7 @@ namespace CatchButton
 
             UpdateTitle();
 
-            MessageBox.Show("축하합니다~!", "성공");
+            MessageBox.Show("축하합니다~!\n성공했어요!", "성공");
         }
 
         private void CheckGameOver()
@@ -143,8 +152,9 @@ namespace CatchButton
                 isGameOver = true;
                 buttonCatch.Enabled = false;
                 buttonReset.Enabled = true;
+                buttonCatch.BackColor = Color.Gray;
 
-                MessageBox.Show("Game Over");
+                MessageBox.Show("Game Over\n다시 시작 버튼을 눌러 재도전하세요.", "게임 오버");
             }
         }
 
@@ -159,19 +169,43 @@ namespace CatchButton
             buttonCatch.Location = new Point(180, 160);
 
             UpdateTitle();
+            Invalidate();
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            if (buttonCatch.Right > this.ClientSize.Width)
-                buttonCatch.Left = Math.Max(0, this.ClientSize.Width - buttonCatch.Width);
-
-            int maxTop = Math.Max(0, this.ClientSize.Height - buttonCatch.Height - 70);
-
-            if (buttonCatch.Top > maxTop)
-                buttonCatch.Top = maxTop;
-
             UpdateTitle();
+            Invalidate();
+        }
+
+        // 배경 색상 지정
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Rectangle rect = ClientRectangle;
+            using LinearGradientBrush gradient = new LinearGradientBrush(
+                rect,
+                Color.FromArgb(35, 20, 80),
+                Color.FromArgb(0, 180, 255),
+                35f);
+            e.Graphics.FillRectangle(gradient, rect);
+
+            using SolidBrush overlay = new SolidBrush(Color.FromArgb(55, 255, 255, 255));
+            foreach (Point point in backgroundDots)
+            {
+                int size = random.Next(10, 26);
+                e.Graphics.FillEllipse(overlay, point.X, point.Y, size, size);
+            }
+
+            using LinearGradientBrush titleBrush = new LinearGradientBrush(
+                new Rectangle(15, 15, 280, 50),
+                Color.FromArgb(255, 255, 255),
+                Color.FromArgb(255, 230, 120),
+                0f);
+            using Font titleFont = new Font("맑은 고딕", 18F, FontStyle.Bold);
         }
     }
 }
